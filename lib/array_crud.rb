8c7ef1073c985d893d0f563a95954135548d7403
Colin Sidberry <colinsidberry@gmail.com>
2019-09-18 16:59:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,17 +8,17 @@
 def create_an_empty_array
   []
 end
-
+#-----------------------------
 def create_an_array
-  
+  [1,2,3,4]
 end
-
+#-----------------------------
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
-
+#-----------------------------
 def add_element_to_start_of_array(array, element)
-  
+  arr 
 end
 
 def remove_element_from_end_of_array(array)
