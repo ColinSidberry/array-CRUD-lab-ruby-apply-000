e257e204e7b3fa3218efd3c4569365b3985da756
Colin Sidberry <colinsidberry@gmail.com>
2019-09-18 16:54:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,10 @@
+#1. Create a new array that contains data as well as a new array that is empty.
+2. Add an element to the end of an array.
+3. Add an element to the front of an array.
+4. Remove an element from the front of an array.
+5. Remove an element from the end of an array.
+6. Retrieve data from a specific array index.
+
 def create_an_empty_array
   
 end
